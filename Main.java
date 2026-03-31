3a22c57d80851b89cfc10746b7cedcc2207458a8
BobFett3185 <kdferdkids@gmail.com>
2026-03-31 14:04:13 -0500

    added main

--- Main.java
+++ Main.java
@@ -1,12 +1,35 @@
-
+import java.io.File;
+import java.util.Scanner;
 
 public class Main { // Or whatever name you specify in your README [cite: 109]
     public static void main(String[] args) {
+
+        String inputFilepath, outputFilepath; 
+        inputFilepath = args[0]; // get the input file path from the command line 
+        outputFilepath = args[1]; // get the output file path 
+        
+        File inputFile = new File(inputFilepath); 
+        File outputFile = new File(outputFilepath);
+
+        // Create a Scanner object for the input file
+        Scanner scanner = new Scanner(inputFile);
+
         LazyBinarySearchTree tree = new LazyBinarySearchTree();
 
-        // make some input and output files
-        String[] inputFiles = {"input1.txt", "input2.txt", "input3.txt"};
-        String[] outputFiles = {"output1.txt", "output2.txt", "output3.txt"};
+        while(scanner.hasNext()){
+            String line = scanner.nextLine(); 
+            if (line.startsWith("Insert"))
+                tree.insert(Integer.parseInt(line.substring(7))); // get the number after "Insert " and insert it into the tree
+            else if (line.startsWith("Delete"))
+                tree.delete(Integer.parseInt(line.substring(7))); // get the number after "Delete " and delete it from the tree
+            else if (line.startsWith("Contains"))
+                tree.contains(Integer.parseInt(line.substring(9))); // get the number after "Contains " and check if it's in the tree
+        }
+
+        scanner.close();
+
+
+        
         
 
 
